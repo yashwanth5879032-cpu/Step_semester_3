d5c22f05396a30d171fbd03fe83977bc92277f67
Mitikiri Yashwanth <yashwanth5879032@gmail.com>
2026-08-29 11:46:11 +0530

    added problems

---
+++ step_semester_3/src/main/java/session_3/session_3prg1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class session_3prg1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+
+        if (age >= 18)
+            System.out.println("Eligible to vote");
+        else
+            System.out.println("Not eligible to vote");
+    }
+}
+
+void main() {
+}
