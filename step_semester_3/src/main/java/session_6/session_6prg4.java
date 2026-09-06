7516bf503db10638fe45e44812830c7e73c889a4
Mitikiri Yashwanth <yashwanth5879032@gmail.com>
2026-09-06 15:43:53 +0530

    added

---
+++ step_semester_3/src/main/java/session_6/session_6prg4.java
@@ -0,0 +1,48 @@
+public class session_6prg4 {
+
+    static double rowAverage(int[] row) {
+
+        int sum = 0;
+
+        for (int value : row) {
+            sum += value;
+        }
+
+        return (double) sum / row.length;
+    }
+
+    static String classifyMatches(int[][] runsPerOver, int threshold) {
+
+        String result = "";
+
+        for (int i = 0; i < runsPerOver.length; i++) {
+
+            double avg = rowAverage(runsPerOver[i]);
+
+            if (avg >= threshold) {
+                result += "Match " + i + ": Power Surge";
+            } else {
+                result += "Match " + i + ": Normal";
+            }
+
+            if (i < runsPerOver.length - 1) {
+                result += " | ";
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] runs = {
+                {4, 6, 8},
+                {10, 12, 14},
+                {2, 3, 1}
+        };
+
+        int threshold = 8;
+
+        System.out.println(classifyMatches(runs, threshold));
+    }
+}
